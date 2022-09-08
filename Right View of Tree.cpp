20e218457aa13b16254cf2d587e7ef210aa3774d
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2022-09-08 12:20:10 +0530

    Right View of Tree

---
+++ Right View of Tree.cpp
@@ -0,0 +1,27 @@
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        
+        if(root == NULL)
+            return ans;
+        queue<pair<TreeNode*, int>> q;
+        map<int, int> m;
+        
+        q.push({root,0});
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            TreeNode* node = it.first;
+            int line = it.second;
+            
+            m[line] = node->val;
+            
+            if(node->left) q.push({node->left, line+1});
+            if(node->right) q.push({node->right, line+1});
+        }
+        for(auto it:m)
+            ans.push_back(it.second);
+        return ans;
+    }
+};
