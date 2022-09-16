35c978b1da0e1153f800448c4f74539b194b15f3
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2022-09-16 19:18:43 +0530

    0-1 Knapsack/0-1 Knapsack Memoization

--- Dynamic Programming Aditya Verma Sir/0-1 Knapsack/0-1 Knapsack Memoization.cpp
+++ Dynamic Programming Aditya Verma Sir/0-1 Knapsack/0-1 Knapsack Memoization.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
+int t[102][1002]
 int Knapsack(int wt[], int val[], int W, int n){
     if(n == 0 || W == 0)
         return 0;
+    if(t[n][w] != -1)
+        return t[n][W];
     if(wt[n-1]<=W)
-        return val[n-1] + max(Knapsack(wt, val, W - wt[n-1], n-1), Knapsack(wt, val, W, n-1));
+        return t[n][W] =  val[n-1] + max(Knapsack(wt, val, W - wt[n-1], n-1), Knapsack(wt, val, W, n-1));
     if(wt[n-1] > W)
-        return Knapsack(wt, val, W, n-1);
+        return t[n][W] = Knapsack(wt, val, W, n-1);
 }
 int main(){
     int n;
